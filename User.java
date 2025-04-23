05c2f7b9fea004b79c799bf739064627e235e368
Rommana44 <omaryousefromman1@gmail.com>
2025-04-23 16:53:43 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,17 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
-/**
- *
- * @author lenovo
- */
 public class User {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
          String name;
     }
